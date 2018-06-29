d693b397361c63bccbfd8dcbee7a7ac47b4e0719
Mike Stewart <dungeonmesher@gmail.com>
2018-06-29 14:59:45 -0400

    Added a timer for the player's view

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,87 +5,87 @@ $( document ).ready(function() {
     
     // This object's format makes it easy to display to the screen
     var triviaQuestions = {
-        "question1":"thing1",
-        "question2":"thing2",
-        "question3":"thing3",
-        "question4":"thing4",
-        "question5":"thing5",
-        "question6":"thing6",
-        "question7":"thing7",
-        "question8":"thing8",
-        "question9":"thing9",
-        "question10":"thing10",
+        "question1":"This is Question 1",
+        "question2":"This is Question 2",
+        "question3":"This is Question 3",
+        "question4":"This is Question 4",
+        "question5":"This is Question 5",
+        "question6":"This is Question 6",
+        "question7":"This is Question 7",
+        "question8":"This is Question 8",
+        "question9":"This is Question 9",
+        "question10":"This is Question 10",
     }
     
     // Multiple choice selections should be objects for adding to the DOM
     var trivQuesAnswers1 = {
-        "answer1":"ansmwer1",
-        "answer2":"ansmwer2",
-        "answer3":"ansmwer3",
-        "answer4":"ansmwer4",
+        "answer1":"Q1 A1",
+        "answer2":"Q1 A2",
+        "answer3":"Q1 A3",
+        "answer4":"Q1 A4",
     }
     
     var trivQuesAnswers2 = {
-        "answer1":"ansmwer1",
-        "answer2":"ansmwer2",
-        "answer3":"ansmwer3",
-        "answer4":"ansmwer4",
+        "answer1":"Q2 A1",
+        "answer2":"Q2 A2",
+        "answer3":"Q2 A3",
+        "answer4":"Q2 A4",
     }
     
     var trivQuesAnswers3 = {
-        "answer1":"ansmwer1",
-        "answer2":"ansmwer2",
-        "answer3":"ansmwer3",
-        "answer4":"ansmwer4",
+        "answer1":"Q3 A1",
+        "answer2":"Q3 A2",
+        "answer3":"Q3 A3",
+        "answer4":"Q3 A4",
     }
     
     var trivQuesAnswers4 = {
-        "answer1":"ansmwer1",
-        "answer2":"ansmwer2",
-        "answer3":"ansmwer3",
-        "answer4":"ansmwer4",
+        "answer1":"Q4 A1",
+        "answer2":"Q4 A2",
+        "answer3":"Q4 A3",
+        "answer4":"Q4 A4",
     }
     
     var trivQuesAnswers5 = {
-        "answer1":"ansmwer1",
-        "answer2":"ansmwer2",
-        "answer3":"ansmwer3",
-        "answer4":"ansmwer4",
+        "answer1":"Q5 A1",
+        "answer2":"Q5 A2",
+        "answer3":"Q5 A3",
+        "answer4":"Q5 A4",
     }
     
     var trivQuesAnswers6 = {
-        "answer1":"ansmwer1",
-        "answer2":"ansmwer2",
-        "answer3":"ansmwer3",
-        "answer4":"ansmwer4",
+        "answer1":"Q6 A1",
+        "answer2":"Q6 A2",
+        "answer3":"Q6 A3",
+        "answer4":"Q6 A4",
     }
     
     var trivQuesAnswers7 = {
-        "answer1":"ansmwer1",
-        "answer2":"ansmwer2",
-        "answer3":"ansmwer3",
-        "answer4":"ansmwer4",
+        "answer1":"Q7 A1",
+        "answer2":"Q7 A2",
+        "answer3":"Q7 A3",
+        "answer4":"Q7 A4",
     }
     
     var trivQuesAnswers8 = {
-        "answer1":"ansmwer1",
-        "answer2":"ansmwer2",
-        "answer3":"ansmwer3",
-        "answer4":"ansmwer4",
+        "answer1":"Q8 A1",
+        "answer2":"Q8 A2",
+        "answer3":"Q8 A3",
+        "answer4":"Q8 A4",
     }
     
     var trivQuesAnswers9 = {
-        "answer1":"ansmwer1",
-        "answer2":"ansmwer2",
-        "answer3":"ansmwer3",
-        "answer4":"ansmwer4",
+        "answer1":"Q9 A1",
+        "answer2":"Q9 A2",
+        "answer3":"Q9 A3",
+        "answer4":"Q9 A4",
     }
     
     var trivQuesAnswers10 = {
-        "answer1":"ansmwer1",
-        "answer2":"ansmwer2",
-        "answer3":"ansmwer3",
-        "answer4":"ansmwer4",
+        "answer1":"Q10 A1",
+        "answer2":"Q10 A2",
+        "answer3":"Q10 A3",
+        "answer4":"Q10 A4",
     }
     
     function game () {
@@ -107,37 +107,25 @@ $( document ).ready(function() {
     
     // When the game starts, question(s) will appear, and the timer should begin
         // The timer between rounds should be 2 minutes, then the game ends
-        var gameTimer = setTimeout(endGame, 120 * 1000);
-    
-    //     <form >
-    // <input type="radio" name="choice" value="Scripting"> Scripting
-    // <input type="radio" name="choice" value="Programming"> Programming
-    // <input type="radio" name="choice" value="Application"> Application
-    // <input type="radio" name="choice" value="None of These"> None of These
-    // </form>
-    
-    // for (choiceNum = 1; choiceNum <= 4; choiceNum++) {
-    //     ques1Choices.append(`<input type="radio" name="choice" value="` + )
-    // }
+        var gameTimeout = setTimeout(endGame, 120 * 1000);
+
+
+        // The game should last 2 minutes (120 seconds)
+        var remainingTime = 120;
+
+
+        // Decrease the timer for the game 
+        var intervalId = setInterval(function() {
+            remainingTime--;
+            // Show the remaining time to the player
+            $("#timerDisplay").html("Remaining Time: " + remainingTime);
+        }, 1000);
     
     
         // Add the trivia questions
         var question1 = $("<p>");
         question1.attr("id", "question1");
         question1.text(triviaQuestions.question1)
-    
-        // var ques1Choices = $("<form>");
-        // ques1Choices.attr("id", "ques1Choices");
-        // Object.values(trivQuesAnswers1).forEach(function(choice) {
-        //     ques1Choices.append(`<input type="radio" name="choice" value="` + choice + `">` + choice)
-        //     //ques1Choices.html(`<input type="radio" name="choice" value="` + choice + `">` + choice)
-        //  });
-    
-    
-        //ques1Choices.html('<input type="radio" name="choice" value="meatball">Meatball')
-        //`<input type="radio" name="choice" value="` + choice + `">` + choice
-        // console.log(Object.values(trivQuesAnswers1))
-        // console.log(ques1Choices)
     
         var question2 = $("<p>");
         question2.attr("id", "question2");
@@ -176,39 +164,21 @@ $( document ).ready(function() {
         question10.text(triviaQuestions.question10)
     
     
-    
-    Object.keys(triviaQuestions).forEach(function(trivQuesKey) {
-      var ques1Choices = $("<form>");
-      ques1Choices.attr("id", "ques1Choices");
-      Object.values(trivQuesAnswers1).forEach(function(choice) {
-          ques1Choices.append(`<input type="radio" name="choice" value="` + choice + `">` + choice)
-          //ques1Choices.html(`<input type="radio" name="choice" value="` + choice + `">` + choice)
-       });
-    
-    
-        console.log('trivQuesKey', trivQuesKey)
-        // console.log('ques 1 choices ', ques1Choices)
-        $("#questionSection").append(eval(trivQuesKey));
-        $("#" + trivQuesKey).append(ques1Choices)
-    });
-    
-    
-    // Object.keys(triviaQuestions).forEach(function(trivQuesKey) {
-    //     $("#questionSection").append(eval(trivQuesKey));
-    //     $("#questionSection").append("<p>Test</p>")
-    // })
-    
-    // Object.keys(triviaQuestions).forEach(function(trivQuesKey) {
-    //     $("#questionSection").append(eval(trivQuesKey));
-    //     for (b = 1; b <= 10; b++) {
-    //         $("#question" + b).append("<p>Test</p>")
-    //     }
-    // });
-    
-    // Object.keys(triviaQuestions).forEach(function(trivQuesKey) {
-    //     $("#questionSection").append(eval(trivQuesKey));
-    //         $("#question" + (Object.keys(triviaQuestions).indexOf(trivQuesKey) + 1)).append("<p>Test</p>")
-    // });
+    // All the questions & choices should programmatically display for the player
+        //Pull each one from the object of Trivia Questions
+        Object.keys(triviaQuestions).forEach(function(trivQuesKey) {
+        // Build the HTML form for the radio buttons and each one should have unique ID assigned
+            var quesChoices = $("<form>");
+            quesChoices.attr("id", "choices-" + trivQuesKey);
+            // Display the radio buttons with answers directly below the question
+            Object.values(trivQuesAnswers1).forEach(function(choice) {
+                quesChoices.append(`<input type="radio" name="choice" value="` + choice + `">` + choice);
+            });
+            // Display each Trivia Question to the DOM for the player to see
+            $("#questionSection").append(eval(trivQuesKey));
+            // Match the answers to the corresponding question
+            $("#" + trivQuesKey).append(quesChoices)
+        });
     
     
     
@@ -218,7 +188,8 @@ $( document ).ready(function() {
         doneButton.attr("id", "doneButton");
         $("#pageFooter").append(doneButton)
         $("#doneButton").on("click", endGame)
-    }
+
+    } // End of function game()
     
     function endGame () {
         console.log("time ran out")
